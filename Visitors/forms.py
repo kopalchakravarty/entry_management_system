acf9f870d90c39d92aa932f74d146cf368e443ce
kopalchakravarty <31576619+kopalchakravarty@users.noreply.github.com>
2019-11-28 01:31:10 +0530

    Update forms.py
    Added comments

--- Visitors/forms.py
+++ Visitors/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import *
 
+# DEFINES FIELDS THAT NEED TO BE FILLED BY THE USER 
 
-class VisitorForm(forms.ModelForm):
+class VisitorForm(forms.ModelForm):  # VISITOR FIELDS
     class Meta:
         model=Visitors
         fields=('name','phone','email','checkin','checkout')
 
 
-class HostForm(forms.ModelForm):
+class HostForm(forms.ModelForm):  # HOST FIELDS
     class Meta:
         model=Host
         fields=('name','phone','email')
